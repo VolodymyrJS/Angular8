9308dbcca823a084e215c382cd0f8cba5c63d8e6
Volodymyr Ponomarenko <volodymyr.ponomarenko@extrawest.com>
2019-12-05 16:17:18 +0200

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular8';
+  expectedString = 'recipe';
+
+  onEmmitedFeature(feature: string) {
+    this.expectedString = feature;
+  }
 }
